cb7b8be6c80d03e6593b18725e19ad6172f2afcd
Magesh <tkmagesh77@gmail.com>
2018-07-12 14:16:51 +0530

    async demo - solution

--- 11-async/async-demo.js
+++ 11-async/async-demo.js
@@ -18,11 +18,20 @@ function f4Sync(){
 	console.log('f4Sync completed');
 }
 
-function runSync(){
+/*function runSync(){
 	f1Sync();
 	f2Sync();
 	f3Sync();
 	f4Sync();
+}*/
+
+var syncFns = [f1Sync, f2Sync, f3Sync, f4Sync];
+
+function runSync(){
+	for(var index = 0, count = syncFns.length; index < count; index++){
+		var syncFn = syncFns[index];
+		syncFn();
+	}
 }
 
 module.exports.runSync = runSync;
@@ -63,11 +72,40 @@ function f4Async(next){
 	}, 3000);
 }
 
+/*function runAsync(){
+	f1Async(function(){
+		f2Async(function(){
+			f3Async(function(){
+				f4Async();
+			});
+		});
+	});
+}*/
+
+var asyncFns = [f1Async, f2Async, f3Async, f4Async];
+
 function runAsync(){
-	f1Async();
-	f2Async();
-	f3Async();
-	f4Async();
+	function exec(fns){
+		var first = fns[0],
+			remaining = fns.slice(1),
+			next = function(){
+				exec(remaining);
+			};
+		if (typeof first === 'function')
+			first(next);
+	}
+	exec(asyncFns);
 }
 
+
+
 module.exports.runAsync = runAsync;
+
+
+
+
+
+
+
+
+
