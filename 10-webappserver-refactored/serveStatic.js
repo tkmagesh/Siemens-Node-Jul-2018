c5242d1f6dc58238d06877239454255b426bccb8
Magesh <tkmagesh77@gmail.com>
2018-07-12 14:56:42 +0530

    webappserver - complete

--- 10-webappserver-refactored/serveStatic.js
+++ 10-webappserver-refactored/serveStatic.js
@@ -7,34 +7,18 @@ function isStatic(resourceName){
 	return staticResExtns.indexOf(path.extname(resourceName)) !== -1;
 }
 
-module.exports = function(req, res,  next){
-	var resourceName = req.urlObj.pathname === '/' ? 'index.html' : req.urlObj.pathname;
-
-	if (isStatic(resourceName)){
-		var resourceFullName = path.join(__dirname, resourceName);
-		if (!fs.existsSync(resourceFullName)){
-			console.log('serving 404 from serverStatic');
-			res.statusCode = 404;
-			res.end();
-			return;
+module.exports = function(resourcePath){
+	return function(req, res,  next){
+		var resourceName = req.urlObj.pathname === '/' ? 'index.html' : req.urlObj.pathname;
+		var resourceFullName = path.join(resourcePath, resourceName);
+		if (isStatic(resourceFullName) && fs.existsSync(resourceFullName)){
+			var stream = fs.createReadStream(resourceFullName);
+			stream.pipe(res);
+			stream.on('end', function(){
+				next();
+			});
+		} else {
+			next();
 		}
-		//fs.createReadStream(resourceFullName).pipe(res);
-		/*var stream = fs.createReadStream(resourceFullName);
-		stream.on('data', function(chunk){
-			console.log('start serving file chunk to response @ serveStatic');
-			res.write(chunk);
-		});
-		stream.on('end', function(){
-			console.log('ending response @ serveStatic');
-			res.end();
-			next();
-		})*/
-		var stream = fs.createReadStream(resourceFullName);
-		stream.pipe(res);
-		stream.on('end', function(){
-			next();
-		});
-	} else {
-		next();
-	}
-} 
+	};
+};
